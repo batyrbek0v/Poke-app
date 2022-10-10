371ec25bd8c207fcf7b70fe7debf93cf45da6a99
unknown <erlankambarov94@gmail.com>
2022-10-10 11:45:58 +0600

    added cardTemplate and styles for cards

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,11 @@
 
+// DOM-ELEMENTS
+
+const $container = document.querySelector('.container')
+const $wrapper = document.querySelector('.wrapper')
+
+
+// ==================================================
 
 const POKE_BASE = 'https://pokeapi.co/api/v2/'
 
@@ -12,7 +19,7 @@ window.addEventListener('load', () => {
 	getPokemons
 		(
 			`${POKE_BASE}pokemon`, `limit=${limitOfPokemons}&offset=${offsetCounter}`, cb => {
-				console.log(cb);
+				cardTemplate(cb.results)
 			}
 		)
 })
@@ -22,4 +29,23 @@ const getPokemons = (url, query, callback) => {
 	fetch(`${url}?${query}`)
 		.then(res => res.json())
 		.then(res => callback(res))
+}
+
+const cardTemplate = (pokemons) => {
+	const template = pokemons.map(({ name, url }) => `
+	<div class="card">
+      <div class="card_title">
+        <h2>${name} ${offsetCounter}</h2>
+      </div>
+      <div class="card_body">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1280px-International_Pok%C3%A9mon_logo.svg.png">
+      </div>
+      <div class="card_footer">
+        <button onclick="getSinglePokemon('${url}')" class="moreBtn">Go to pokedex</button>
+      </div>
+    </div>
+  `).join('')
+	$wrapper.innerHTML = template
+
+	console.log(pokemons);
 }
